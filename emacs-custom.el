72e037f272c5cf088081e22736695a950df7a918
ein <ein@mousenull>
2025-02-11 13:20:40 +0900

    updated.

--- emacs-custom.el
+++ emacs-custom.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(bash-completion browse-kill-ring cargo ccls clang-format cmake-mode coffee-mode company-dict counsel ddskk dockerfile-mode eglot elm-mode elpy emmet-mode f flycheck flycheck-pyflakes flymake god-mode gradle-mode graphviz-dot-mode groovy-mode haskell-mode howm idomenu js2-mode lsp-haskell lsp-java lsp-mode lsp-pyright lsp-ui lua-mode magit markdown-mode navi2ch nginx-mode plantuml-mode powershell purescript-mode restclient rust-mode shakespeare-mode slime solarized-theme swiper tidal treemacs typescript-mode undo-tree vue-mode web-mode wgrep xclip yaml-mode yasnippet yasnippet-classic-snippets yasnippet-snippets))
+   '(bash-completion browse-kill-ring cargo ccls clang-format cmake-mode coffee-mode company-dict counsel ddskk direnv dockerfile-mode eglot eglot-java elm-mode elpy emmet-mode envrc f flycheck flycheck-pyflakes flymake god-mode gradle-mode graphviz-dot-mode groovy-mode haskell-mode howm idomenu js2-mode lsp-haskell lsp-java lsp-mode lsp-pyright lsp-ui lua-mode magit markdown-mode navi2ch nginx-mode plantuml-mode powershell purescript-mode restclient rust-mode shakespeare-mode slime solarized-theme swiper tidal treemacs typescript-mode undo-tree vue-mode web-mode wgrep xclip yaml-mode yasnippet yasnippet-classic-snippets yasnippet-snippets))
  '(safe-local-variable-values
    '((eval setq-local lsp-pyright-venv-path
            (expand-file-name "~/.venvs")))))
